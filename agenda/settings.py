a32375db87f6edc094fa797d0c342477b5432ece
Maroc <marcos.valentee20@gmail.com>
2021-08-28 15:01:08 -0400

    adicionando imagem no perfil do usuario

--- agenda/settings.py
+++ agenda/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'teste',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'wasionbr',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -131,3 +131,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
